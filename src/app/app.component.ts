bd86dda185e050cb5a1fcfc8af9ee8849e604ddf
staudytorres <staudytorres@gmail.com>
2024-03-18 22:12:22 -0500

    Hola2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'bases';
+  public title:string = 'Hola mundo Angular';
+  public counter:number = 10;
+
+  increaseBy(value:number):void {
+  this.counter+=value;
+  }
+  decreaseBy(value:number):void{
+    this.counter+=value;
+    }
+    reset():void{
+      this.counter=10;
+      }
 }
