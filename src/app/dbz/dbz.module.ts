a90a21dd9216319203c753cbfe70210555f6eebc
staudytorres <staudytorres@gmail.com>
2024-03-21 01:22:09 -0500

    Se agrego el modulo dbz

---
+++ src/app/dbz/dbz.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainComponent } from './pages/main-page.component';
+
+
+
+@NgModule({
+  declarations: [MainComponent],exports:[MainComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class DbzModule {
+
+
+
+ }
